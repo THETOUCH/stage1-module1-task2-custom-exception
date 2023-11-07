d4d042d4f6895fdfe25f28730de951fada6eb635
THETOUCH <super.ilgiznex@gmail.com>
2023-11-07 09:41:08 +0600

    123

---
+++ src/main/java/com/epam/mjc/exception/StudentNotFoundException.java
@@ -0,0 +1,6 @@
+package com.epam.mjc.exception;
+public class StudentNotFoundException extends IllegalArgumentException {
+    public StudentNotFoundException(String message) {
+        super(message);
+    }
+}
